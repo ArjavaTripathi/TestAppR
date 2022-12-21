b45e652009eced67393eb452f866f6c5cd9406d3
ArjavaTripathi <arjavatripathi21@singhaniaschools.com>
2022-12-21 13:48:19 +0530

    Initial Commit

---
+++ Test-AppR/Test_AppRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppRApp.swift
+//  Test-AppR
+//
+//  Created by Arjava Tripathi on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
